5c2fa8cdbdfbf829b612669956b8f7b029352833
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-26 22:19:43 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Abu Sayeed Roni on 2023-08-26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
